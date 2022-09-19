1a503046b53af7a5f4efe59044c4c9acaf21eb70
Jallller <cph-jl398@cphbusiness.dk>
2022-09-19 14:53:14 +0200

    opretter getConnection med scanner

--- src/Niveau3_Persistens/ConnectionConfig.java
+++ src/Niveau3_Persistens/ConnectionConfig.java
@@ -1,5 +1,29 @@
 package Niveau3_Persistens;
 
-public class ConnectionConfig {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
 
+public class ConnectionConfig {
+    public static Connection getConnection() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Skriv user:");
+        String user = sc.next();
+        System.out.println("Skriv password");
+        String password = sc.next();
+        System.out.println("user: " + user + " : " + "password: " + password);
+
+        Connection connection = null;
+        String url = "jdbc:mysql://localhost:3306/demodemo?serverTimezone=CET&useSSL=false";
+
+//        //Default;
+//        String user = "root";
+//        String password = "AskildogKonrad";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
